999d57748df337308d88177ce959af7684f6365e
joon <joon@mpi-inf.mpg.de>
2017-04-20 12:23:10 +0200

    segmentation test code

--- src/segmentation/config.py
+++ src/segmentation/config.py
@@ -88,3 +88,118 @@ def config_train(control, conf, EXP_PHASE):
     pprint.pprint(control)
 
     return control, control_token, control_sup_token, conf
+
+
+def config_test(control, conf, EXP_PHASE):
+    assert (EXP_PHASE == 'seg-test')
+
+    defaults = dict(
+        init='VGG_ILSVRC_16_layers-deeplab',
+        net='DeepLab',
+        dataset='voc12train_aug',
+        datatype='Segmentation',
+        base_lr=0.001,
+        batch_size=15,
+
+        # seed
+        s_g_init='VGG_ILSVRC_16_layers',
+        s_g_dataset='voc12train_aug',
+        s_g_datatype='Segmentation',
+        s_g_base_lr=0.001,
+        s_g_batch_size=15,
+        s_g_balbatch='clsbal',
+        s_g_test_iter=8000,
+        s_g_test_dataset='voc12train_aug',
+        s_g_test_datatype='Segmentation',
+        s_g_test_ranking='none',
+        s_g_test_gtcls='use',
+
+        # SAL
+        s_s_net='DeepLabv2_ResNet',
+        s_s_dataset='MSRA',
+        s_s_datatype='NP',
+        s_s_test_dataset='voc12train_aug',
+        s_s_test_datatype='Segmentation',
+
+        s_gtcls='use',
+        s_seedthres=20,
+        s_salthres=50,
+        s_test_dataset='voc12train_aug',
+        s_test_datatype='Segmentation',
+
+        test_iter=8000,
+        test_datatype='Segmentation',
+        test_pcrf='none',
+    )
+
+    defaults_model = dict(
+        init='VGG_ILSVRC_16_layers-deeplab',
+        net='DeepLab',
+        dataset='voc12train_aug',
+        datatype='Segmentation',
+        base_lr=0.001,
+        batch_size=15,
+
+        # seed
+        s_g_init='VGG_ILSVRC_16_layers',
+        s_g_dataset='voc12train_aug',
+        s_g_datatype='Segmentation',
+        s_g_base_lr=0.001,
+        s_g_batch_size=15,
+        s_g_balbatch='clsbal',
+        s_g_test_iter=8000,
+        s_g_test_dataset='voc12train_aug',
+        s_g_test_datatype='Segmentation',
+        s_g_test_ranking='none',
+        s_g_test_gtcls='use',
+
+        # SAL
+        s_s_net='DeepLabv2_ResNet',
+        s_s_dataset='MSRA',
+        s_s_datatype='NP',
+        s_s_test_dataset='voc12train_aug',
+        s_s_test_datatype='Segmentation',
+
+        s_gtcls='use',
+        s_seedthres=50,
+        s_salthres=50,
+        s_test_dataset='voc12train_aug',
+        s_test_datatype='Segmentation',
+    )
+
+    exclude_from_model = [
+        'test_iter',
+        'test_dataset',
+        'test_datatype',
+        'test_pcrf',
+    ]
+
+    control_model = control.copy()
+    for ky in defaults_model:
+        if control_model[ky] == defaults_model[ky]:
+            control_model.pop(ky)
+
+    for exc in exclude_from_model:
+        if exc in control_model.keys():
+            control_model.pop(exc)
+
+    control_token = control.copy()
+    for ky in defaults:
+        if control_token[ky] == defaults[ky]:
+            control_token.pop(ky)
+
+    conf['EXP_PHASE'] = EXP_PHASE
+
+    conf['input_size'] = 321
+    conf['output_size'] = 41
+    if 'voc' in control['dataset']:
+        conf['nposcls'] = 20
+    else:
+        raise NotImplementedError
+
+    load_pascal_conf(control, conf)
+
+    pprint.pprint(conf)
+    pprint.pprint(control)
+
+    return control, control_model, control_token, conf
--- src/segmentation/test.py
+++ src/segmentation/test.py
@@ -13,20 +13,21 @@ from imports.import_caffe import *
 from imports.ResearchTools import *
 from imports.libmodules import *
 
+from config import config_test
 from networks import deeplab
-from config import config_train
 
 ####
 
-EXP_PHASE = 'seg-train'
+EXP_PHASE = 'seg-test'
 
 conf = dict(
-    vis=False,
     save=True,
+    vis=False,
+    shuffle=True,
     overridecache=True,
     pascalroot="/BS/joon_projects/work/",
     imagenetmeanloc="data/ilsvrc_2012_mean.npy",
-    gpu=3,
+    gpu=1,
 )
 
 control = dict(
@@ -67,16 +68,28 @@ control = dict(
     # s_guiderule='salor',
     s_test_dataset='voc12train_aug',
     s_test_datatype='Segmentation',
+
+    test_iter=8000,
+    test_dataset='voc12val',
+    test_datatype='Segmentation',
+    test_pcrf='deeplab',
 )
 
 
 ####
 
 def parse_input(argv=sys.argv):
-    parser = argparse.ArgumentParser(description="Trains a segmentation network")
-    parser.add_argument('--init', default='VGG_ILSVRC_16_layers', type=str,
+    parser = argparse.ArgumentParser(description="Tests a segmentation network")
+    parser.add_argument('--test_iter', default=8000, type=int,
                         help='Initialisation for the network')
-    parser.add_argument('--net', default='GAP-HighRes', type=str,
+    parser.add_argument('--test_dataset', default='voc12val', type=str,
+                        help='Test set')
+    parser.add_argument('--test_datatype', default='Segmentation', type=str,
+                        help='Type of test set')
+
+    parser.add_argument('--init', default='VGG_ILSVRC_16_layers-deeplab', type=str,
+                        help='Initialisation for the network')
+    parser.add_argument('--net', default='DeepLab', type=str,
                         help='Network')
     parser.add_argument('--dataset', default='voc12train_aug', type=str,
                         help='Training set')
@@ -89,7 +102,7 @@ def parse_input(argv=sys.argv):
 
     parser.add_argument('--s_g_init', default='VGG_ILSVRC_16_layers', type=str,
                         help='Initialisation for the network')
-    parser.add_argument('--s_g_net', default='DeepLab', type=str,
+    parser.add_argument('--s_g_net', default='GAP-HighRes', type=str,
                         help='Network')
     parser.add_argument('--s_g_dataset', default='voc12train_aug', type=str,
                         help='Training set')
@@ -147,76 +160,153 @@ def parse_input(argv=sys.argv):
     parser_conf.add_argument('--vis', default=False, type=bool,
                              help='Visualisation')
     parser_conf.add_argument('--save', default=True, type=bool,
-                             help='Cache intermediate and final results')
+                             help='Save raw heatmap output')
     parser_conf.add_argument('--overridecache', default=True, type=bool,
                              help='Override cache')
+    parser_conf.add_argument('--shuffle', default=True, type=bool,
+                             help='Shuffle test input order')
     conf = vars(parser_conf.parse_known_args(argv)[0])
     return control, conf
 
 
-def write_proto(trainproto, testproto, conf, control):
-    f = open(trainproto, 'w')
-    f.write(deeplab(conf, control, 'train'))
-    f.close()
+def write_proto(testproto, conf, control):
     f = open(testproto, 'w')
     f.write(deeplab(conf, control, 'test'))
     f.close()
     return
 
 
-def write_solver(solverproto, trainproto, testproto, learnedmodel_dir, control, conf):
-    solverprototxt = tools.CaffeSolver(trainnet_prototxt_path=trainproto, testnet_prototxt_path=testproto)
-    solverprototxt.sp['test_interval'] = "100000000"
-    solverprototxt.sp['test_iter'] = "1"
-
-    solverprototxt.sp['lr_policy'] = '"step"'
-    solverprototxt.sp['gamma'] = "0.1"
-    solverprototxt.sp['base_lr'] = str(control['base_lr'])
-    solverprototxt.sp['display'] = "10"
-    solverprototxt.sp['stepsize'] = conf['training']['stepsize']
-    solverprototxt.sp['max_iter'] = conf['training']['max_iter']
-    solverprototxt.sp['snapshot'] = conf['training']['snapshot']
-    solverprototxt.sp['momentum'] = "0.9"
-    solverprototxt.sp['weight_decay'] = "0.0005"
-    solverprototxt.sp['snapshot_prefix'] = '"' + learnedmodel_dir + '/"'
-    solverprototxt.sp['solver_mode'] = "GPU"
-    solverprototxt.write(solverproto)
-
-    return
-
-
-def load_solver(control, control_token, conf):
-    learnedmodel_dir = osp.join('cache', EXP_PHASE, create_token(control_token))
-    mkdir_if_missing(learnedmodel_dir)
-    print('saving to: {}'.format(learnedmodel_dir))
-
+def setup_net(control, control_model, control_token):
     # prototxt
     protodir = osp.join('models', EXP_PHASE, create_token(control_token))
     mkdir_if_missing(protodir)
-    trainproto = osp.join(protodir, 'train.prototxt')
     testproto = osp.join(protodir, 'test.prototxt')
-    solverproto = osp.join(protodir, 'solver.prototxt')
-    write_proto(trainproto, testproto, conf, control)
-    write_solver(solverproto, trainproto, testproto, learnedmodel_dir, control, conf)
+    write_proto(testproto, conf, control)
+
+    learnedmodel_dir = osp.join('cache', 'seg-train', create_token(control_model))
+    learnedmodel = osp.join(learnedmodel_dir, '_iter_' + str(control['test_iter']) + '.caffemodel')
 
     # init
     caffe.set_mode_gpu()
     caffe.set_device(conf['gpu'])
-    solver = caffe.SGDSolver(solverproto)
-    initmodel = osp.join('data', EXP_PHASE, control['init'] + '.caffemodel')
-    solver.net.copy_from(initmodel)
-    disp_net(solver.net)
-    print('saving to: {}'.format(learnedmodel_dir))
 
-    return solver
+    net = caffe.Net(testproto, learnedmodel, caffe.TEST)
+    disp_net(net)
+
+    return net
+
+
+def run_test(net, out_dir, control, conf):
+    year = '20' + control['test_dataset'][3:5]
+    pascal_list = get_pascal_indexlist(conf['pascalroot'], year, control['test_datatype'], control['test_dataset'][5:],
+                                       shuffle=conf['shuffle'])
+
+    num_test = len(pascal_list)
+    print('%d images for testing' % num_test)
+
+    transformer = set_preprocessor_without_net(
+        [1, 3, conf['input_size'], conf['input_size']],
+        mean_image=np.load(conf['imagenetmeanloc']).mean(1).mean(1))
+
+    pred_list = []
+    id_list = []
+
+    start_time = time.time()
+    for idx in range(num_test):
+        end_time = time.time()
+        print ('    Iter %d took %2.1f seconds' % (idx, end_time - start_time))
+        start_time = time.time()
+        print ('    Running %d out of %d images' % (idx + 1, num_test))
+
+        inst = idx
+        im_id = pascal_list[inst]
+        outfile = osp.join(out_dir, im_id + '.mat')
+
+        if conf['save_heat']:
+            if not conf['overridecache']:
+                if osp.isfile(outfile):
+                    print('skipping')
+                    continue
+
+        gtfile = conf['clsimgpath'] % (im_id)
+        gt = np.array(Image.open(gtfile)).astype(np.float)
+
+        imloc = os.path.join(conf['pascalroot'], 'VOC' + year, 'JPEGImages', im_id + '.jpg')
+        image = load_image_PIL(imloc)
+        imshape_original = image.shape[:2]
+
+        net.blobs['data'].data[...][0] = preprocess_convnet_image(image, transformer, 321, 'test')
+        net.forward()
+
+        scoremap = net.blobs['scoremap'].data[0]
+        scoremap_originalshape = nd.zoom(scoremap, [1, imshape_original[0] / float(conf['output_size']),
+                                                    imshape_original[1] / float(conf['output_size'])], order=1)
+        seg_raw = scoremap_originalshape.argmax(0)
+
+        if control['test_pcrf'] != 'none':
+            probmap_originalshape = Jsoftmax(scoremap_originalshape, axis=0)
+            probmap_originalshape_smth = CRF(image, probmap_originalshape, crf_param=control['test_pcrf'])
+            seg = probmap_originalshape_smth.argmax(0).astype(np.uint8)
+        else:
+            seg = seg_raw.copy().astype(np.uint8)
+
+        if conf['vis']:
+
+            def visualise_data():
+                fig = plt.figure(0, figsize=(15, 10))
+                fig.suptitle('ID:{}'.format(im_id))
+                ax = fig.add_subplot(2, 6, 1)
+                ax.set_title('Original image')
+                ax.imshow(image)
+                ax = fig.add_subplot(2, 6, 4)
+                ax.imshow(image)
+                ax.imshow(gt, alpha=.5, cmap="nipy_spectral", clim=(0, 30))
+
+                ax = fig.add_subplot(2, 6, 2)
+                ax.set_title('Raw output')
+                ax.imshow(seg_raw, cmap="nipy_spectral", clim=(0, 30))
+                ax = fig.add_subplot(2, 6, 5)
+                ax.imshow(image)
+                ax.imshow(seg_raw, alpha=.5, cmap="nipy_spectral", clim=(0, 30))
+
+                ax = fig.add_subplot(2, 6, 3)
+                ax.set_title('Postprocessed')
+                ax.imshow(seg, cmap="nipy_spectral", clim=(0, 30))
+                ax = fig.add_subplot(2, 6, 6)
+                ax.imshow(image)
+                ax.imshow(seg, alpha=.5, cmap="nipy_spectral", clim=(0, 30))
+
+                for iii in range(6):
+                    fig.axes[iii].get_xaxis().set_visible(False)
+                    fig.axes[iii].get_yaxis().set_visible(False)
+
+                plt.pause(1)
+                return
+
+            visualise_data()
+
+        if conf['save']:
+            if not conf['overridecache']:
+                assert (not os.path.isfile(outfile))
+            else:
+                if os.path.isfile(outfile):
+                    print('WARNING: OVERRIDING EXISTING RESULT FILE')
+            cv2.imwrite(outfile, seg)
+            print('results saved to %s' % outfile)
+    return
 
 
 def main(control, conf):
-    control, control_token, control_sup_token, conf = config_train(control, conf, EXP_PHASE)
-    conf['sup_dir'] = osp.join('cache', 'guide-generate', create_token(control_sup_token))
-    solver = load_solver(control, control_token, conf)
-    solver.step(int(conf['training']['max_iter']))
-    del solver
+    control, control_model, control_token, conf = config_test(control, conf, EXP_PHASE)
+
+    out_dir = osp.join('cache', EXP_PHASE, create_token(control_token))
+    mkdir_if_missing(out_dir)
+    print('saving to: {}'.format(out_dir))
+
+    net = setup_net(control, control_model, control_token)
+
+    run_test(net, out_dir, control, conf)
+    return
 
 
 if __name__ == '__main__':
