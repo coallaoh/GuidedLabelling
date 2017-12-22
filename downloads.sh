a16d1f67fd23b97c95f150b288d333acbd314ef2
Seong Joon Oh <joon@mpi-inf.mpg.de>
2017-12-22 10:29:53 +0100

    train_aug.txt added

--- downloads.sh
+++ downloads.sh
@@ -4,3 +4,7 @@
 wget https://transfer.d2.mpi-inf.mpg.de/joon/joon17cvpr/data.tar.gz
 tar xvf data.tar.gz
 rm data.tar.gz
+
+wget https://transfer.d2.mpi-inf.mpg.de/joon/joon17cvpr/list.tar.gz
+tar xvf list.tar.gz
+rm list.tar.gz
